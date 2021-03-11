3dd6b92e8cc004bf0870f9aca85eb168742e80fa
ShawnFrost23 <arthpatelf50@gmail.com>
2021-03-11 17:35:46 +0530

    Added Custom button component

---
+++ frontend/src/components/CustomButton/CustomButton.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  withStyles,
+} from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const ColorButton = withStyles((theme) => ({
+  root: {
+    width: '80%',
+    backgroundColor: '#161a25',
+    color: 'white',
+    borderColor: 'white',
+    fontSize: 16,
+    padding: '5px 35px',
+    margin: '15px',
+    '&:hover': {
+      backgroundColor: '#5ca933',
+      borderColor: '#5ca933',
+    },
+  },
+
+}))(Button);
+
+function CustomButton({ displayText, func }) {
+  return (
+    <ColorButton variant="outlined" color="primary" size="large" onClick={func} >{displayText}</ColorButton>
+  )
+}
+
+export default CustomButton
