b112fe121fdaf5ccbef22af8c6b17048e690813a
ShawnFrost23 <arthpatelf50@gmail.com>
2021-03-14 19:01:58 +0530

    Added top nav bar to advance home screen

--- frontend/src/pages/AdvanceHomeScreen/AdvanceHomeScreen.jsx
+++ frontend/src/pages/AdvanceHomeScreen/AdvanceHomeScreen.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import TopNavBar from '../../components/TopNavBar/TopNavBar';
 
 import styles from './AdvanceHomeScreen.module.css';
 function AdvanceHomeScreen() {
     return (
-        <div className={styles.container}>
-            Advance Home Screen Content
-        </div>
+        <>
+            <TopNavBar />
+            <div className={styles.container}>
+                Advance Home Screen Content
+            </div>
+        </>
     )
 }
 
