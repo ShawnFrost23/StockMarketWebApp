e7301cda3a38e920f51d00252360cf77c8cd7221
ShawnFrost23 <arthpatelf50@gmail.com>
2021-03-14 18:25:17 +0530

    Basic React functional class for advancehomescreen

---
+++ frontend/src/pages/AdvanceHomeScreen/AdvanceHomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import styles from './AdvanceHomeScreen.module.css';
+function AdvanceHomeScreen() {
+    return (
+        <div className={styles.container}>
+            Advance Home Screen Content
+        </div>
+    )
+}
+
+export default AdvanceHomeScreen
