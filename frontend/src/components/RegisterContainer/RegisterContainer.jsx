b56f2e0fb782ffcd95fc793aa2553ab9b83cc2f9
ShawnFrost23 <arthpatelf50@gmail.com>
2021-03-14 15:08:57 +0530

    added email format validation for registeration

--- frontend/src/components/RegisterContainer/RegisterContainer.jsx
+++ frontend/src/components/RegisterContainer/RegisterContainer.jsx
@@ -43,6 +43,14 @@ function RegisterContainer() {
               setEmailHelpText('Enter your Email ');
             return false;
         }
+        // Email format validation
+        if (typeof (email) !== "undefined") {
+            var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+            if (!pattern.test(email)) {
+              setEmailHelpText('Enter Valid Email');
+              return false;
+            }
+        }
         return true;
     }
 
