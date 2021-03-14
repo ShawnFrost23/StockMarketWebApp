21f91374949d20b04df87aea67c3b81d355132fe
ShawnFrost23 <arthpatelf50@gmail.com>
2021-03-14 10:52:46 +0530

    used state for event handling

--- frontend/src/components/RegisterContainer/RegisterContainer.jsx
+++ frontend/src/components/RegisterContainer/RegisterContainer.jsx
@@ -6,7 +6,19 @@ import CustomTextField from '../CustomTextField/CustomTextField';
 import CustomButton from '../CustomButton/CustomButton';
 import LogRegHeading from '../LogRegHeading/LogRegHeading';
 function RegisterContainer() {
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [confirmPassword, setConfirmPassword] = React.useState('');
+
     const history = useHistory();
+
+    const handleRegister = () => {
+        console.log("🚀 ~ file: RegisterContainer.jsx ~ line 10 ~ RegisterContainer ~ name", name)
+        console.log("🚀 ~ file: RegisterContainer.jsx ~ line 12 ~ RegisterContainer ~ email", email)
+        console.log("🚀 ~ file: RegisterContainer.jsx ~ line 14 ~ RegisterContainer ~ password", password)
+        console.log("🚀 ~ file: RegisterContainer.jsx ~ line 16 ~ RegisterContainer ~ confirmPassword", confirmPassword)
+    }
 
     const loginButtonHandler = () => {
         history.push('loginScreen');
@@ -18,18 +30,25 @@ function RegisterContainer() {
             />
             <CustomTextField 
                 placeholder="Name"
+                setValue={setName}
             />
             <CustomTextField 
                 placeholder="Email"
+                setValue={setEmail}
             />
             <CustomTextField 
                 placeholder="Password"
+                type='password'
+                setValue={setPassword}
             />
             <CustomTextField 
                 placeholder="Confirm Password"
+                type='password'
+                setValue={setConfirmPassword}
             />
             <CustomButton
                 displayText="Register"
+                func={handleRegister}
             />
             <CustomButton
                 displayText="User? Login Now!"
