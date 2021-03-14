f3e608b15967116503fb188ff5a6dfc1cd053c25
ShawnFrost23 <arthpatelf50@gmail.com>
2021-03-14 11:57:57 +0530

    Added verification code variables

--- frontend/src/components/PasswordResetContainer/PasswordResetContainer.jsx
+++ frontend/src/components/PasswordResetContainer/PasswordResetContainer.jsx
@@ -5,9 +5,11 @@ import CustomTextField from '../CustomTextField/CustomTextField';
 import CustomButton from '../CustomButton/CustomButton';
 import LogRegHeading from '../LogRegHeading/LogRegHeading';
 function PasswordResetContainer() {
+    // Verification Code State Variables
     const [veriCode, setVeriCode] = React.useState(0);
     const [veriCodeErr, setVeriCodeErr] = React.useState(false);
     const [veriCodeHelpText, setVeriCodeHelpText] = React.useState('');
+    
     const [newPass, setNewPass] = React.useState('');
     const [confirmNewPass, setConfirmNewPass] = React.useState('');
 
