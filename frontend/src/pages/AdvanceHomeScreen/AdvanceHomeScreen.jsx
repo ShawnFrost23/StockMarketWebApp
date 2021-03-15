5e0675ebfb71615822ceaec69bca2a13ac4c5718
ShawnFrost23 <arthpatelf50@gmail.com>
2021-03-15 09:53:13 +0530

    State variables for watchlists

--- frontend/src/pages/AdvanceHomeScreen/AdvanceHomeScreen.jsx
+++ frontend/src/pages/AdvanceHomeScreen/AdvanceHomeScreen.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import TopNavBar from '../../components/TopNavBar/TopNavBar';
 
 import styles from './AdvanceHomeScreen.module.css';
+
 function AdvanceHomeScreen() {
+    const [watchLists, setWatchLists] = React.useState([]);
+    
     return (
         <>
             <TopNavBar />
