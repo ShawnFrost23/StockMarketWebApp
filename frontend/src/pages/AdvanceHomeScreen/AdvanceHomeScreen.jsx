9d1f68930ef6510166bd91eda41119c0c9ce4979
ShawnFrost23 <arthpatelf50@gmail.com>
2021-03-16 11:23:10 +0530

    Added top bar to home screen

--- frontend/src/pages/AdvanceHomeScreen/AdvanceHomeScreen.jsx
+++ frontend/src/pages/AdvanceHomeScreen/AdvanceHomeScreen.jsx
@@ -8,7 +8,8 @@ function AdvanceHomeScreen() {
     const [watchLists, setWatchLists] = React.useState([]);
     
     return (
-        <>
+        <>  
+            <TopNavBar />
             <WatchlistContainer />
         </>
     )
