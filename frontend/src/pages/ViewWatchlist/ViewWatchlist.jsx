4b089de354bda1b97b451282c367cb4b2e899ec3
ShawnFrost23 <arthpatelf50@gmail.com>
2021-03-16 11:25:00 +0530

    Added Top Nav Bar for Watchlist Screens

--- frontend/src/pages/ViewWatchlist/ViewWatchlist.jsx
+++ frontend/src/pages/ViewWatchlist/ViewWatchlist.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import TopNavBar from '../../components/TopNavBar/TopNavBar';
 import ViewWatchlistContainer from '../../components/ViewWatchlistContainer/ViewWatchlistContainer';
 
 // import styles from './ViewWatchlist.module.css';
 function ViewWatchlist() {
     return (
         <>
+            <TopNavBar />
             <ViewWatchlistContainer />
         </>
     )
