80812c6d993106796584f229bc9ee09d4cf8d8d5
ShawnFrost23 <arthpatelf50@gmail.com>
2021-03-20 12:38:04 +0530

    complete watchlistcard component

--- frontend/src/components/WatchListCards/WatchListCards.jsx
+++ frontend/src/components/WatchListCards/WatchListCards.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styles from './WatchListCards.module.css';
 
-function WatchListCards() {
+import AssessmentIcon from '@material-ui/icons/Assessment';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+function WatchListCards({ id, watchListName, viewFunc, editFunc, deleteFunc}) {
     return (
-        <div>
-            
+        <div key={id} className={styles.container}>
+            <div className={styles.headingName}>{watchListName}</div>
+            <AssessmentIcon style={{color: 'green'}} onClick={viewFunc}/>
+            <EditIcon style={{color: 'blue'}} onClick={editFunc}/>
+            <DeleteIcon style={{color: 'red'}} onClick={deleteFunc}/>
         </div>
     )
 }
 
-export default WatchListCards
+export default WatchListCards;
