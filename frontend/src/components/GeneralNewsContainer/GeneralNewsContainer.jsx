24cca09afc7a943baa7aa73118ec4fb7a98037f4
ShawnFrost23 <arthpatelf50@gmail.com>
2021-03-25 12:26:15 +0530

    Updated code to work with CORS

--- frontend/src/components/GeneralNewsContainer/GeneralNewsContainer.jsx
+++ frontend/src/components/GeneralNewsContainer/GeneralNewsContainer.jsx
@@ -1,25 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import styles from './GeneralNewsContainer.module.css';
 
 // API KEY for newsapi: 6f3b269cd1974ca58522d326e9556f0c
 
 function GeneralNewsContainer() {
-    const NewsAPI = require('newsapi');
-    const newsapi = new NewsAPI('6f3b269cd1974ca58522d326e9556f0c');
+    const [newsList, setNewsList] = useState([]);
 
     async function getNews(queryName) {
         const response = await fetch(`https://newsapi.org/v2/everything?q=${queryName}&language=en&sortBy=publishedAt&apiKey=6f3b269cd1974ca58522d326e9556f0c`)
-        // const response = await newsapi.v2.topHeadlines({
-        //     sources: 'bbc-news,the-verge',
-        //     q: 'bitcoin',
-        //     category: 'business',
-        //     language: 'en',
-        //     country: 'us'
-        //   })
         const body = await response.json();
-        if (body) {
-            console.log("🚀 ~ file: GeneralNewsContainer.jsx ~ line 22 ~ getNews ~ response", body)
+        if (body.status === 'ok') {
+            console.log("🚀 ~ file: GeneralNewsContainer.jsx ~ line 16 ~ getNews ~ response", body)
+
         }
     }
 
