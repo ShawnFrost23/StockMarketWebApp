c7bd317730ee572a0a5d5d8eaf9ccfc03a1538ff
ShawnFrost23 <arthpatelf50@gmail.com>
2021-03-25 12:19:32 +0530

    Basic news card functional class

---
+++ frontend/src/components/GeneralNewsCard/GeneralNewsCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import styles from './GeneralNewsCard.module.css';
+function GeneralNewsCard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default GeneralNewsCard
