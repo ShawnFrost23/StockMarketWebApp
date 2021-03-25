e942251b2233932f34b6eb6b6e47d5ff6a361dce
ShawnFrost23 <arthpatelf50@gmail.com>
2021-03-25 13:01:57 +0530

    Partially working news container

--- frontend/src/components/GeneralNewsContainer/GeneralNewsContainer.jsx
+++ frontend/src/components/GeneralNewsContainer/GeneralNewsContainer.jsx
@@ -1,25 +1,43 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import { useHistory } from 'react-router-dom';
+import GeneralNewsCard from '../GeneralNewsCard/GeneralNewsCard';
 
 import styles from './GeneralNewsContainer.module.css';
 
 // API KEY for newsapi: 6f3b269cd1974ca58522d326e9556f0c
 
 function GeneralNewsContainer() {
-    const [newsList, setNewsList] = useState([]);
+    const [newsList, setNewsList] = useState();
+    const history = useHistory();
 
     async function getNews(queryName) {
         const response = await fetch(`https://newsapi.org/v2/everything?q=${queryName}&language=en&sortBy=publishedAt&apiKey=6f3b269cd1974ca58522d326e9556f0c`)
         const body = await response.json();
-        if (body.status === 'ok') {
-            console.log("🚀 ~ file: GeneralNewsContainer.jsx ~ line 16 ~ getNews ~ response", body)
-
-        }
+        const articles = await body.articles;
+        console.log("🚀 ~ file: GeneralNewsContainer.jsx ~ line 16 ~ getNews ~ response", articles)
+        setNewsList(articles[0]);
     }
+
+    useEffect(() => {
+        const displayNews = async () => {
+            getNews('Google');
+        }
+
+        if (localStorage.getItem('user_id') === null) {
+            history.push('/loginScreen');
+        }
+
+        displayNews();
+    }, [history])
 
     return (
         <div className={styles.container}>
             News Container
-            {getNews('Amazon')}
+            {/* {newsList.map((article) => ( */}
+                {/* <GeneralNewsCard 
+                    newsArticle={newsList}
+                /> */}
+            {/* ))} */}
         </div>
     )
 }
