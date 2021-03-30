febe6ac1e4a51f32e7271d5bd1a10caa9d07b764
Donna Zhou <13839920+dondonz@users.noreply.github.com>
2021-03-30 12:49:10 +1100

    Add last close and price change

--- backend/server/asset.py
+++ backend/server/asset.py
@@ -4,8 +4,12 @@ def overview(ticker):
     # TODO must connect with Dan's ticker validation or this will 500
     stock = yf.Ticker(f"{ticker}.AX")
     last_price = stock.history(period="1d", interval="1m").iloc[-1]["Close"]
+    last_close = stock.history(period="2d", interval="1d").iloc[-2]["Close"]
     return { "last_price": last_price,
+             "change": last_price - last_close,
+             "change_percent": (last_price - last_close) * 100 / last_close,
+             "volume": stock.info["volume"],
+             "market_cap": stock.info["marketCap"],
              "fiftyTwoWeekHigh": stock.info["fiftyTwoWeekHigh"],
              "fiftyTwoWeekLow": stock.info["fiftyTwoWeekLow"],
-             "volume": stock.info["volume"],
-             "market_cap": stock.info["marketCap"]}
+           }
