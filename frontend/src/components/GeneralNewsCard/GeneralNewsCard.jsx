de888bdb33d6897bd8b8b1a729daab884a146aa6
ShawnFrost23 <arthpatelf50@gmail.com>
2021-03-31 11:17:01 +0530

    added basic details regarding news

--- frontend/src/components/GeneralNewsCard/GeneralNewsCard.jsx
+++ frontend/src/components/GeneralNewsCard/GeneralNewsCard.jsx
@@ -6,6 +6,15 @@ function GeneralNewsCard( { newsArticle } ) {
     return (
         <div className={styles.container}>
             <h5>{newsArticle.author}</h5>
+            <div className={styles.newsTitle}>
+                {newsArticle.title}
+            </div>
+            <div className={styles.newsImage}>
+                <img className={styles.newsImage} src={newsArticle.urlToImage} alt="Image"/>
+            </div>
+            <div className={styles.newsSourced}>
+                Source <a href={newsArticle.url} target="_blank">{newsArticle.source.name}</a>
+            </div>
         </div>
     )
 }
