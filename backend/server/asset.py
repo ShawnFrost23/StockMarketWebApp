f3b7ea3704097588586ebae2ed0478e8e20fb85a
Donna Zhou <13839920+dondonz@users.noreply.github.com>
2021-04-04 09:21:43 +1000

    Add company name to asset overview

--- backend/server/asset.py
+++ backend/server/asset.py
@@ -1,6 +1,8 @@
 import yfinance as yf
 import psycopg2
 
+from .watchlist import *
+
 def connect():
     con = psycopg2.connect(database="iteration1", user="diamond_hands", password="1234", host="127.0.0.1", port="5432")
     cur = con.cursor()
@@ -13,6 +15,7 @@ def overview(asset_id):
     ticker = result[2]
 
     stock = yf.Ticker(f"{ticker}.AX")
+    company_name = validate(ticker)['company_name']
     last_price = stock.history(period="1d", interval="1m").iloc[-1]["Close"]
     last_close = stock.history(period="2d", interval="1d").iloc[-2]["Close"]
 
@@ -35,12 +38,13 @@ def overview(asset_id):
     return { "last_price": round(last_price, 2),
              "daily_nominal_change": daily_nominal_change,
              "daily_percentage_change": daily_percentage_change,
-             "volume": stock.info["volume"],
-             "market_cap": stock.info["marketCap"],
              "weekly_nominal_change": weekly_nominal_change,
              "weekly_percentage_change": weekly_percentage_change,
              "monthly_nominal_change": monthly_nominal_change,
              "monthly_percentage_change": monthly_percentage_change,
              "yearly_nominal_change": yearly_nominal_change,
              "yearly_percentage_change": yearly_percentage_change,
+             "volume": stock.info["volume"],
+             "market_cap": stock.info["marketCap"],
+             "company_name": company_name,
            }
