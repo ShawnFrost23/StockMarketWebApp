4145d59601a0158f4b6f318457bacad7cf529334
diuleilomo <z5172514@unsw.edu.au>
2021-04-18 17:25:37 +1000

    backend done

--- backend/server/prediction.py
+++ backend/server/prediction.py
@@ -18,65 +18,87 @@ sell_signal_list = {
     "Hanging Man": "CDLHANGINGMAN",
     "Shooting Star": "CDLSHOOTINGSTAR",
     "Evening Star": "CDLEVENINGSTAR",
-    "Three Black Crows":"CDL3BLACKCROWS"
+    "Three Black Crows":"CDL3BLACKCROWS",
+    "Three-Line Strike": "CDL3LINESTRIKE"
 }
 
+cont_signal_list = {
+    "Doji": "CDLDOJI",
+    "Spinning Top": "CDLSPINNINGTOP",
+    "Rising/Falling Three Methods": "CDLRISEFALL3METHODS",
+    "Tasuki Gap": "CDLTASUKIGAP",
+    "Separating Lines": "CDLSEPARATINGLINES"
+}
 
 
 def stock_price(ticker):
 
     stock = yf.Ticker(f"{ticker}.AX")
-    price = stock.history(period="1y", interval="1d")
+    price = stock.history(period="3mo", interval="1d")
 
     return price 
 
 
 def buy_signal(price):
-    result = []
+    count = 0
 
     for pattern in buy_signal_list:
         data = eval("talib." + buy_signal_list[pattern] + "(price['Open'], price['High'], price['Low'], price['Close'])")
         data = data[data != 0]
         
         if (len(data) > 0):
-            print(pattern)
-            print(data)
-            result.append(data.index.tolist())
+            count += len(data.tolist())
     
-
-    result.sort(reverse=True)
-    return result
+    return count
 
 def sell_signal(price):
 
-    result = []
+    count = 0
 
     for pattern in sell_signal_list:
         data = eval("talib." + sell_signal_list[pattern] + "(price['Open'], price['High'], price['Low'], price['Close'])")
         data = data[data != 0]
         
         if (len(data) > 0):
-            print(pattern)
-            print(data)
-            result.append(data.index.tolist())
+            count += len(data.tolist())
+   
+    return count
+
+
+def cont_signal(price):
+
+    count = 0
+
+    for pattern in sell_signal_list:
+        data = eval("talib." + sell_signal_list[pattern] + "(price['Open'], price['High'], price['Low'], price['Close'])")
+        data = data[data != 0]
+        
+        if (len(data) > 0):
+            count += len(data.tolist())
     
-
-    result.sort(reverse=True)
-    return result
+    return count
 
 
-
-
-def control_flow(ticker):
+def predict(ticker):
 
     price = stock_price(ticker)
-    buy_dates = buy_signal(price)
-    sell_dates = sell_signal(price)
-    print(buy_dates)
-    print(sell_dates)
+    buy = buy_signal(price)
+    sell = sell_signal(price)
+    cont = cont_signal(price)
+
+    if (buy >= cont and buy > sell):
+        signal = "Buy"
+    elif (sell >= cont and sell > buy):
+        signal = "Sell"
+    else:
+        signal = "Hold"
+
+
+    return {"signal": signal, "buy": buy, "sell": sell, "hold": cont}
 
 
 
-control_flow('XRO')
+
+print(predict('APT'))
 
 
