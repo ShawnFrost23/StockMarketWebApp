b50398ea1d0de914532853b1ecff4a3c3ce3dbde
Donna Zhou <13839920+dondonz@users.noreply.github.com>
2021-04-19 20:08:08 +1000

    Connect signals to frontend

--- backend/server/asset.py
+++ backend/server/asset.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import psycopg2
 
 from .watchlist import *
+from .prediction import *
 
 def connect():
     con = psycopg2.connect(database="iteration1", user="diamond_hands", password="1234", host="127.0.0.1", port="5432")
@@ -39,6 +40,8 @@ def overview(asset_id):
     forward_PE = round(stock.info["forwardPE"], 2)
     trailing_PE = round(stock.info["trailingPE"], 2)
 
+    predictions = predict(ticker)
+
     return { "last_price": round(last_price, 2),
              "daily_nominal_change": daily_nominal_change,
              "daily_percentage_change": daily_percentage_change,
@@ -56,4 +59,8 @@ def overview(asset_id):
              "dividend_yield": dividend_yield,
              "forward_PE": forward_PE,
              "trailing_PE": trailing_PE,
+             "predictions_signal": predictions["signal"],
+             "predictions_buy": predictions["buy"],
+             "predictions_sell": predictions["sell"],
+             "predictions_hold": predictions["hold"],
            }
