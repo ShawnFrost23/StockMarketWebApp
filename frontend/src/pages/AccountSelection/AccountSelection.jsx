17b5f079cd259a221011bd94fef643b64023c61a
ShawnFrost23 <arthpatelf50@gmail.com>
2021-04-19 19:29:46 +0530

    Basic Account Selection Screen

---
+++ frontend/src/pages/AccountSelection/AccountSelection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import styles from './AccountSelection.module.css';
+function AccountSelection() {
+    return (
+        <div className={styles.container}>
+            Account Selection Screen
+        </div>
+    )
+}
+
+export default AccountSelection
