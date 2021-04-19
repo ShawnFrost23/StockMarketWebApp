833993f5d160333c41542fb2078151ae05ef4381
ShawnFrost23 <arthpatelf50@gmail.com>
2021-04-19 19:23:34 +0530

    Added background to edit watchlist

--- frontend/src/pages/EditWatchlist/EditWatchlist.jsx
+++ frontend/src/pages/EditWatchlist/EditWatchlist.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import EditWatchlistContainer from '../../components/EditWatchlistContainer/EditWatchlistContainer';
 import TopNavBar from '../../components/TopNavBar/TopNavBar';
 
-// import styles from './EditWatchlist.module.css';
+import styles from './EditWatchlist.module.css';
 function EditWatchlist() {
     return (
-        <div>
+        <div className={styles.container}>
             <TopNavBar />
             <EditWatchlistContainer />
         </div>
