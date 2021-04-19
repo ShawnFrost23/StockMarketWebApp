8ba62ce461c2091748edf4281df9d744a9a50c20
ShawnFrost23 <arthpatelf50@gmail.com>
2021-04-19 11:21:31 +0530

    refactored code for better styling

--- frontend/src/pages/AdvanceHomeScreen/AdvanceHomeScreen.jsx
+++ frontend/src/pages/AdvanceHomeScreen/AdvanceHomeScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import GeneralNewsContainer from '../../components/GeneralNewsContainer/GeneralNewsContainer';
+import SearchBar from '../../components/SearchBar/SearchBar';
 import TopNavBar from '../../components/TopNavBar/TopNavBar';
 import WatchlistContainer from '../../components/WatchlistContainer/WatchlistContainer';
 
@@ -12,8 +13,18 @@ function AdvanceHomeScreen() {
     return (
         <>
             <TopNavBar />
-            <WatchlistContainer />
-            <GeneralNewsContainer />
+            <div>
+                <div className={styles.companySearch}>
+                    <h1>
+                        Search for a Company
+                    </h1>
+                    <SearchBar />
+                </div>
+                <div className={styles.watchListandNewsContainer}>
+                    <WatchlistContainer />
+                    <GeneralNewsContainer />
+                </div>
+            </div>
         </>
     )
 }
