df0642af0621bf23b9ecd31cf67d6b3401996442
ShawnFrost23 <arthpatelf50@gmail.com>
2021-04-20 19:18:18 +0530

    functional basic home screen

--- frontend/src/pages/BasicHomeScreen/BasicHomeScreen.jsx
+++ frontend/src/pages/BasicHomeScreen/BasicHomeScreen.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import TopNavBar from '../../components/TopNavBar/TopNavBar';
+import SearchBar from '../../components/SearchBar/SearchBar';
 
 import styles from './BasicHomeScreen.module.css';
+import GeneralNewsCard from '../../components/GeneralNewsCard/GeneralNewsCard';
+
+// API KEY for newsapi: 6f3b269cd1974ca58522d326e9556f0c
 function BasicHomeScreen() {
+    const [newsList, setNewsList] = useState([]);
+
+    async function getGeneralNews () {
+        const companyFullName = 'ASX';
+        const response = await fetch(`https://newsapi.org/v2/everything?q=${companyFullName}&language=en&sortBy=publishedAt&apiKey=6f3b269cd1974ca58522d326e9556f0c`)
+        if (response.status == 429) {
+            setNewsList([]);
+        } else {
+            const body = await response.json();
+            const articles = await body.articles;
+            setNewsList(articles);
+        }
+    }
+
+    useEffect(() => {
+        getGeneralNews();
+    }, []);
+
     return (
         <div className={styles.container}>
-            To Be implemented.
+            <TopNavBar 
+                version={1}
+            />
+            <div className={styles.subContainer}>
+                <div className={styles.companySearch}>
+                    <h1>
+                        Search for a Company
+                    </h1>
+                    <SearchBar />
+                </div>
+                <h2>General ASX News</h2>
+                <div className={styles.watchListandNewsContainer}>
+                    <div className={styles.newsContainer}>
+                        {newsList.map((article) => (
+                        <div className={styles.newsCardContainer}>
+                            <GeneralNewsCard 
+                                newsArticle={article}
+                            />
+                        </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
